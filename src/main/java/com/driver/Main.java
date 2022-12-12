e07f37052aedc726afe265e2e67f753b45b97456
Amalvc <amalram301@gmail.com>
2022-12-12 20:02:51 +0530

    mssg

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,21 +2,21 @@ package com.driver;
 
 public class Main {
     public static class Product{
-        public int Product(int x,int y){
+        public int product(int x,int y){
             return x*y;
         }
-        public int Product(int x,int y,int z){
+        public int product(int x,int y,int z){
             return x*y*z;
         }
-        public  double Product(double x,double y){
+        public  double product(double x,double y){
             return x*y;
         }
     }
     public static void main(String[] args) {
         Product p=new Product();
-        System.out.println(p.Product(4,5));
-        System.out.println(p.Product(4,5,8));
-        System.out.println(p.Product(4.2,5.3));
+        System.out.println(p.product(4,5));
+        System.out.println(p.product(4,5,8));
+        System.out.println(p.product(4.2,5.3));
     }
 
 }
